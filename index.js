6908194d9b6fa4fb3ea420e26d83001c29934b85
tadappi <iskyimaging@gmail.com>
2025-11-12 13:06:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@
 
   // ====== 一元サイズ設定 ======
   // ピン画像(※info.png)の見た目サイズ。ここだけ変えればOK
-  var HOTSPOT_SIZE = 80;     // px
-  var LABEL_GAP    = 5;     // ピン右側のラベルまでの隙間(px)
+  var HOTSPOT_SIZE = 60;     // px
+  var LABEL_GAP    = 0;     // ピン右側のラベルまでの隙間(px)
 
   // ====== Viewer ======
   var viewer = new Marzipano.Viewer(panoElement, {
